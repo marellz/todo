29e160ced77ee243be3dbc32dc8e9f4987261eb9
Dave Njoroge <davenjoroge10@gmail.com>
2020-06-05 18:30:19 +0300

    moved data to store

--- front/src/store/index.js
+++ front/src/store/index.js
@@ -5,10 +5,55 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    lists: [
+      {
+        title: "Trip to Paris",
+        bg: '#3A2B85ff',
+        tasks: [
+          { name: "Pack bags", complete:false},
+          { name: "Get VISA", complete: true },
+          { name: "Buy Camera", complete: false},
+          { name: "Book AirBnB", complete: false}
+        ]
+      },
+      {
+        title: "My Tasks",
+        bg: '#E03535ff',
+        tasks: [
+          { name: 'Complete To-Do App', complete:false}
+        ]
+      }
+    ]
+  },
+  getters:{
+    lists(state){
+      return state.lists
+    }
   },
   mutations: {
+    toggleComplete(state,payload){
+      var task = state.lists[payload.list].tasks[payload.task]
+      if(task.complete && task.complete == true){
+        task.complete =  false
+      } else {
+        task.complete = true
+      }
+      // var complete = typeof task.complete == 'boolean' && task.complete == true
+      // task.complete = !complete      
+    },
+    newList(state, payload){
+      state.lists.push(
+        {
+          title: payload.name,
+          bg: '#E03535ff',
+          tasks: [
+          ]
+        }
+      )
+    }
   },
   actions: {
+
   },
   modules: {
   }
