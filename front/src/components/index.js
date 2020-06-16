794d9476d153de78749703ccc11df02a901701fc
Dave Njoroge <davenjoroge10@gmail.com>
2020-06-16 23:42:56 +0300

    prompt component added

--- front/src/components/index.js
+++ front/src/components/index.js
@@ -4,12 +4,14 @@ import FormSelect from './form/Select'
 import FormText from './form/Text'
 import ColorSelector from './form/ColorSelector'
 import Modal from './utils/Modal'
+import Prompt from './utils/Prompt'
 [
     FormInput,
     FormSelect,
     FormText,
     ColorSelector,
     
-    Modal
+    Modal,
+    Prompt,
     
 ].forEach(component => Vue.component(component.name, component))
---
+++ front/src/components/utils/Prompt.vue
@@ -0,0 +1,44 @@
+<template>
+  <modal title="Confirm" @close="close" :active="show">
+    <p>{{action}}</p>
+    <template slot="footer">
+      <button
+        type="button"
+        class="btn btn-secondary"
+        data-dismiss="modal"
+        @click="close"
+      >Cancel</button>
+      <button type="button" @click="confirm" class="btn btn-primary">Yes, I am</button>
+    </template>
+  </modal>
+</template>
+<script>
+export default {
+  name: "Prompt",
+  props: {
+    action: {
+      type: String,
+      default: "Are you sure?"
+    },
+    title: {
+      type: String,
+      default: "Confirmation"
+    },
+    show: {
+      type: Boolean,
+      default: false
+    }
+  },
+
+  methods: {
+    confirm() {
+      this.$emit("confirm");
+    },
+    close(){
+        console.log('clsoe!');
+        
+        this.$emit('close')
+    }
+  }
+};
+</script>
